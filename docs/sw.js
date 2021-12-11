a1db40771b45d696ba1ce9d14a9b7acf5fb22d88
Tre Tatum <tdtatum2@gmail.com>
2021-12-11 17:51:06 -0600

    Monitoring fetches.

--- docs/sw.js
+++ docs/sw.js
@@ -1,16 +1,17 @@
-
-
 self.addEventListener('install', function(event) {
     // Fires when the browser installs the app. Provides space to set up local environment after installation.
+    console.log('SW: Event fire:' + event.type);
 });
 
 
 
 self.addEventListener('activate', function(event) {
     // Fires after service worker completes its installation. Allows service worker to clean up previous versions.
+    console.log('SW: Event activated: ' + event.type);
 });
 
 self.addEventListener('fetch', function(event) {
     // Fires when the app requests files or data.
-    console.log(`SW: Fetching ${event.request.url}`);
+    console.log('SW Fetched.' + event.request.url);
+    event.respondWith(fetch(event.request));
 });
