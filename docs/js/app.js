60bc8ad00c29fac03eac9fcfca5615e2e5983492
Tre Tatum <tdtatum2@gmail.com>
2021-12-11 17:29:58 -0600

    Service worker routing.

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,5 +1,7 @@
 if ('serviceWorker' in navigator) {
+    console.log("In.");
     window.addEventListener("load", () => {
+        console.log("Loaded.");
         navigator.serviceWorker.register('/sw.js')
         .then((reg) => {
             // This message displays when SW registration is successful.
